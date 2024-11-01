77601abe796d60adf23f02227a6b8425b0f0f460
Alex776 <khovailo.alexey@gmail.com>
2024-11-01 16:19:54 +0300

    third dataset checked

--- 5_yolo.py
+++ 5_yolo.py
@@ -3,13 +3,10 @@ import cv2
 from ultralytics.utils.plotting import Annotator  # ultralytics.yolo.utils.plotting is deprecated
 
 model = YOLO('best.pt')
-cap = cv2.imread("C:/Users/user/Downloads/roki 2 photodata/WIN_20241030_10_56_25_Pro (2).jpg")
-
-img = cap
 
     # BGR to RGB conversion is performed under the hood
     # see: https://github.com/ultralytics/ultralytics/issues/2575
-results = model.predict(img)
+results = model.predict(source="C:/Users/user/Downloads/roki 2 photodata", conf=0.6, save=True)
 
 for r in results:
 
@@ -22,7 +19,3 @@ for r in results:
         annotator.box_label(b, model.names[int(c)])
 
     img = annotator.result()
-    while True:
-        cv2.imshow('YOhttps://habr.com/ru/articles/593547/LO V8 Detection', img)
-        if cv2.waitKey(1) & 0xFF == ord(' '):
-            break
